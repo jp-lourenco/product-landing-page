314ea43a747dab8a1eefc44bf262e23f0c778cd8
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-11-10 16:07:33 +0000

    Implemented header container

---
+++ src/container/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Header } from '../components';
+
+const HeaderContainer = () => {
+    return (
+        <Header>
+            <Header.Container>
+                <Header.Nav>
+                    <Header.Logo src={'assets/images/flooop.png'} />
+                    <Header.Menu>
+                        <Header.MenuButton>Home</Header.MenuButton>
+                        <Header.MenuButton>Features</Header.MenuButton>
+                        <Header.MenuButton>About</Header.MenuButton>
+                        <Header.MenuButton>Product</Header.MenuButton>
+                        <Header.MenuButton>Faq</Header.MenuButton>
+                        <Header.MenuButton>Testimonial</Header.MenuButton>
+                        <Header.MenuButton>Contact</Header.MenuButton>
+                    </Header.Menu>
+                </Header.Nav>
+                <Header.Art />
+                <Header.WelcomeContainer>
+                    <Header.WelcomeText>
+                        <Header.Title>
+                            Best & Smart Digital Product
+                        </Header.Title>
+                        <Header.SubTitle>
+                            Donec nec justo eget felis facilisis fermentum.
+                            Aliquam porttitor mauris sit amet orci. Aenean
+                            dignissim pellentesque felis.
+                        </Header.SubTitle>
+                        <Header.BuyButton>Buy product</Header.BuyButton>
+                    </Header.WelcomeText>
+                    <Header.ImageContainer>
+                        <Header.WelcomeImage src={'assets/images/hero.png'} />
+                    </Header.ImageContainer>
+                </Header.WelcomeContainer>
+            </Header.Container>
+        </Header>
+    );
+};
+
+export default HeaderContainer;
