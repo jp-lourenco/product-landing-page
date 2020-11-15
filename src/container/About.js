e4ce0df07e9ea8aae0c60e4af6d85bf9da4b795d
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-11-15 14:22:30 +0000

    Implemented about container

---
+++ src/container/About.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { About } from '../components';
+
+const AboutContainer = () => {
+    return (
+        <About>
+            <About.ImageContainer>
+                <About.Image src={'assets/images/about.png'} />
+            </About.ImageContainer>
+            <About.TextContainer>
+                <About.TitleSection>About Product</About.TitleSection>
+                <About.Title>
+                    Awesome digital product can make your life easier
+                </About.Title>
+                <About.Span />
+                <About.Text>
+                    Donec nec justo eget felis facilisis fermentum. Aliquam
+                    porttitor mauris sit amet orci.
+                </About.Text>
+                <About.Text>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Donec odio. Quisque volutpat mattis eros. Nullam malesuada
+                    erat ut turpis.Donec nec justo eget felis facilisis
+                    fermentum.
+                </About.Text>
+                <About.Ul>
+                    <About.Li>Wireless Connection Headphones</About.Li>
+                    <About.Li>
+                        High-quality audio with DSEE HX™ and LDAC.
+                    </About.Li>
+                    <About.Li>Noise Cancelling without distractions.</About.Li>
+                    <About.Li>Wireless freedom with BLUETOOTH</About.Li>
+                </About.Ul>
+            </About.TextContainer>
+        </About>
+    );
+};
+
+export default AboutContainer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../container/Header';
 import FeatureContainer from '../container/Feature';
+import AboutContainer from '../container/About';
 
 const Home = () => {
     return (
         <>
             <HeaderContainer />
             <FeatureContainer />
+            <AboutContainer />
         </>
     );
 };
